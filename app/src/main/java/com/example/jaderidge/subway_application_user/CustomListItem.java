d17c22d765d8d3fbe5fb96b43edbdc3cf0c5887c
Jaderidge <heyoka1@naver.com>
2017-11-06 03:17:41 +0900

    11-06/03:15 customListView, Switch Listener + file

---
+++ app/src/main/java/com/example/jaderidge/subway_application_user/CustomListItem.java
@@ -0,0 +1,28 @@
+package com.example.jaderidge.subway_application_user;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Jaderidge on 2017-11-06.
+ */
+
+public class CustomListItem {
+    private Drawable icon;
+    private String station;
+
+    public Drawable getIcon(){
+        return icon;
+    }
+
+    public void setIcon(Drawable icon){
+        this.icon = icon;
+    }
+
+    public String getStation(){
+        return station;
+    }
+
+    public void setStation(String station){
+        this.station = station;
+    }
+}
